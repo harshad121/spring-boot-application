1815f9c415a7ec23a833338a7612df9d758c0352
harshad121 <93306490+harshad121@users.noreply.github.com>
2025-08-02 02:10:03 +0530

    Add files via upload

---
+++ HdkApplication.java
@@ -0,0 +1,13 @@
+package com.example.hdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdkApplication.class, args);
+	}
+
+}
---
+++ HdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
